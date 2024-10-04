61c967952406333f8589054ad9bb407eb1bc20c6
Vladyslav Klevanskyi <vladislav.klevanskiy@gmail.com>
2024-10-04 16:42:12 -0400

    Create initial Pydantic schemas

---
+++ schemas.py
@@ -0,0 +1,37 @@
+from pydantic import BaseModel
+from datetime import date
+
+
+class BookBase(BaseModel):
+    title: str
+    summary: str | None = None
+    publication_date: date
+
+
+class BookCreate(BookBase):
+    author_id: int
+
+
+class Book(BookBase):
+    id: int
+    author_id: int
+
+    class Config:
+        orm_model = True
+
+
+class AuthorBase(BaseModel):
+    name: str
+    bio: str | None = None
+
+
+class AuthorCreate(AuthorBase):
+    pass
+
+
+class Author(AuthorBase):
+    id: int
+    books: list[Book] = []
+
+    class Config:
+        orm_model = True
