f925b43b5e6dc9f9f18fd5bb302ef3f0f68af73d
Vladyslav Klevanskyi <vladislav.klevanskiy@gmail.com>
2024-10-06 14:57:41 -0400

    Tablenames were fixed

--- models.py
+++ models.py
@@ -4,22 +4,22 @@ from database import Base
 
 
 class Author(Base):
-    __tablename__ = "author"
+    __tablename__ = "authors"
 
-    id = Column(Integer, primary_key=True)
+    id = Column(Integer, primary_key=True, index=True)
     name = Column(String, unique=True, index=True)
-    bio = Column(String, index=True)
+    bio = Column(String, index=True, nullable=True)
 
     books = relationship("Book", back_populates="author")
 
 
 class Book(Base):
-    __tablename__ = "book"
+    __tablename__ = "books"
 
-    id = Column(Integer, primary_key=True)
-    title = Column(String, index=True)
-    summary = Column(String, index=True)
-    publication_date = Column(Date)
-    author_id = Column(Integer, ForeignKey("author.id"))
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String, nullable=False, index=True)
+    summary = Column(String, nullable=True, index=True)
+    publication_date = Column(Date, nullable=True)
+    author_id = Column(Integer, ForeignKey("authors.id"), nullable=False)
 
     author = relationship("Author", back_populates="books")
